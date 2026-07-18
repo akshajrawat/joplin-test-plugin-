f34f9132a28712c799aba2eb2e39f109e9fc9739
Akshaj Rawat <akshajr11@gmail.com>
2026-07-18 13:03:36 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import joplin from 'api';
 joplin.plugins.register({
 	onStart: async function() {
 		// eslint-disable-next-line no-console
-		console.info('Hello world. Test plugin started!');
+		console.info('this is updated code');
 	},
 });
